be9d5d911f52e07b0d515edffc2834867c913aac
krystal <krystal.low12@gmail.com>
2024-03-31 20:16:28 +0800

    help

--- LSTM.R
+++ LSTM.R
@@ -133,11 +133,23 @@ history <- model %>% fit(
 
 ## UNTIL HERE -ELIZA!!
 
+
+
+
+# Check the shape of X_train
+print(dim(X_train))  # Should match (batch_size, timesteps, rows, columns)
+
+# Check the shape of X_val
+print(dim(X_val))    # Should also match (batch_size, timesteps, rows, columns)
+
+print(dim(y_train))
+
+
 # Create ConvLSTM model
 model <- keras_model_sequential()
 model %>%
   layer_conv_1d(filters = 32, kernel_size = 3, activation = "relu", input_shape = c(5, 1)) %>%
-  #layer_flatten() %>%
+  layer_flatten() %>%
   layer_dense(units = 64, activation = "relu") %>%
   layer_dense(units = 1)
 
@@ -166,3 +178,31 @@ y_pred <- model %>% predict(X_val)
 
 
 
+
+######### USING 2D
+
+X <- scale(X)
+
+X <- array(X, dim = c(n_samples, n_timesteps, n_features, 1))  # Add the channel dimension
+
+# Define the ConvLSTM model
+model <- keras_model_sequential()
+model %>%
+  layer_conv_lstm_2d(filters = 32, kernel_size = 3, input_shape = c(5, 1, 1)) %>%
+  layer_flatten() %>%
+  layer_dense(units = 64, activation = "relu") %>%
+  layer_dense(units = 1)  # Output layer for regression
+
+# Compile the model
+model %>% compile(
+  optimizer = optimizer_adam(),
+  loss = "mean_squared_error"
+)
+
+# Train the model
+model %>% fit(X, y, epochs = 10, batch_size = 32)
+
+# Make predictions
+predictions <- model %>% predict(X)
+
+#########
