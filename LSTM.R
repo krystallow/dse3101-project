a9e82f2f60f1bad71cfff2261f5f418a333d9f89
krystal <krystal.low12@gmail.com>
2024-04-16 19:36:28 +0800

    final

--- LSTM.R
+++ LSTM.R
@@ -2,15 +2,14 @@ library(tidyverse)
 library(stringr)
 library(keras)
 
+# Passenger Volume for Trains - By Origin Destination (2023 Dec)
+pv_train_202312 <- read.csv("../PV Data [Buses and Trains]/Trains/By OD/origin_destination_train_202312.csv")
+
 # Passenger Volume for Trains - By Train Stops (2024 Jan)
 pv_train_202401 <- read.csv("../PV Data [Buses and Trains]/Trains/By OD/origin_destination_train_202401.csv")
 
 # Passenger Volume for Trains - By Train Stops (2024 Feb)
 test_202402 <- read.csv("../PV Data [Buses and Trains]/Trains/By OD/origin_destination_train_202402.csv")
-
-
-# Passenger Volume for Trains - By Origin Destination (2023 Dec)
-pv_train_202312 <- read.csv("../PV Data [Buses and Trains]/Trains/By OD/origin_destination_train_202312.csv")
 
 train_combined <- rbind(pv_train_202312, pv_train_202401, test_202402)
 
@@ -20,7 +19,6 @@ train_combined$YEAR_MONTH <- as.Date(paste0(train_combined$YEAR_MONTH, "-01"))
 summary(train_combined$TOTAL_TRIPS)
 table(train_combined$DAY_TYPE)
 hist(train_combined$TOTAL_TRIPS, breaks=50, main="Distribution of Total Trips")
-
 
 boxplot(TOTAL_TRIPS ~ DAY_TYPE, data=train_combined, main="Total Trips by Day Type")
 
@@ -39,16 +37,14 @@ encode_categorical <- function(df, col_name) {
 
 # Example usage for "DAY_TYPE" column
 train_combined_encoded <- encode_categorical(train_combined, "DAY_TYPE")
-# Apply the same for other categorical columns as needed
+# Apply the same for other categorical columns 
 train_combined_encoded <- encode_categorical(train_combined_encoded, "DESTINATION_PT_CODE")
 train_combined_encoded <- encode_categorical(train_combined_encoded, "ORIGIN_PT_CODE")
 
 # Now, train_combined_encoded has one-hot encoded variables for the specified column
-# You can check the first few rows to see the transformation
+# check the first few rows to see the transformation
 head(train_combined_encoded)
 
-
-# Assuming train_combined_encoded and test_202402_encoded are your current datasets
 
 # Drop the specified columns from the training dataset
 train_combined_encoded <- train_combined_encoded %>%
@@ -87,19 +83,11 @@ X_train_array <- array(X_train_subset, dim = c(nrow(X_train_subset), 1, ncol(X_t
 X_test_array <- array(X_test_subset, dim = c(nrow(X_test_subset), 1, ncol(X_test_subset)))
 
 
-# Define the LSTM model
-model <- keras_model_sequential() %>%
-  layer_lstm(units = 50, input_shape = c(1, ncol(X_train_subset))) %>%
-  layer_dense(units = 1)
-
-
-### NEW
 model <- keras_model_sequential() %>%
   layer_lstm(units = 50, return_sequences = TRUE, input_shape = c(1, ncol(X_train_subset))) %>%
   layer_lstm(units = 30, return_sequences = FALSE) %>% # Additional LSTM layer
   layer_dense(units = 20, activation = 'relu') %>% # An additional dense layer for complexity
   layer_dense(units = 1) # Output layer
-### NEW
 
 
 # Compile the model
@@ -147,40 +135,15 @@ ggplot(loss_data_long, aes(x = Epoch, y = value, color = variable)) +
 
 # Evaluate the model on the test set
 model %>% evaluate(X_test_array, y_test_subset, verbose = 1)
-# loss: 70990.265625 mean_absolute_error: 82.4373092651367
+# loss: 66638.53125 mean_absolute_error:  93.09532 
 
 y_pred <- model %>% predict(X_test_array)   
-
-
-### NEW
 
 predictions <- model %>% predict(X_test_array)
 
 predictions_vector <- as.vector(predictions)
 
 summary(predictions_vector)
-
-df <- data.frame(Predictions = predictions_vector)
-
-# Generate the box plot
-boxplot(df$Predictions,
-        main = "Predictions Box Plot",
-        ylab = "Predicted Values",
-        notch = TRUE,
-        col = "lightblue",
-        border = "darkblue",
-        names = c("Min", "Q1", "Median", "Q3", "Max"))
-
-# Add horizontal lines for quartiles
-abline(h = quantile(predictions_vector, probs = c(0.25, 0.5, 0.75)),
-       col = "red", lty = 2)
-
-# Add legend for quartiles
-legend("topright", legend = c("Q1 (25%)", "Median (50%)", "Q3 (75%)"),
-       col = "red", lty = 2, bty = "n")
-
-
-
 
 # Create a data frame for comparing actual vs. predicted values
 forecast_df <- data.frame(True_Values = y_test_subset, 
@@ -197,38 +160,10 @@ plot(x = forecast_df$Predicted_Values,
 # Add a diagonal line for reference (perfect prediction)
 abline(a = 0, b = 1, col = "red", lwd=3)
 
-library(openxlsx)
-write.xlsx(forecast_df, file = "../new_forecast_LSTM.xlsx", rowNames = FALSE)
-
-# View the first few rows of the comparison
-head(comparison_df)
-# Calculate MAE
-mae <- mean(abs(comparison_df$True_Values - comparison_df$Predicted_Values)) ### mae 86.20542 
-# Calculate RMSE
-rmse <- sqrt(mean((comparison_df$True_Values - comparison_df$Predicted_Values)^2))  ### rmse 255.3118 
-
-cat("Mean Absolute Error: ", mae, "
-")
-cat("Root Mean Squared Error: ", rmse, "
-")
-
-### NEW
-
-
 
 library(Metrics)
-RMSE <- rmse(y_test_subset, y_pred)
-cat("Root Mean Squared Error (RMSE):", round(RMSE, 2), "\n")
-
-residuals <- y_test_subset - y_pred
-
-squared_residuals <- residuals^2
-
-mean_squared_residuals <- mean(squared_residuals)
-
-RMSFE <- sqrt(mean_squared_residuals)
+RMSFE <- sqrt(mean_squared_residuals) #259.87 
 cat("Root Mean Squared Forecast Error (RMSFE):", round(RMSFE, 2), "\n")
-
 
 forecasted_data <- data.frame(   
   Predicted_TOTAL_TRIPS = y_pred,   
@@ -237,87 +172,8 @@ forecasted_data <- data.frame(
 library(openxlsx)
 write.xlsx(forecasted_data, file = "../forecast_LSTM.xlsx", rowNames = FALSE)
 
-# Plot ROC Curve
-library(pROC)
-
-roc_obj <- roc(y_test_subset, y_pred)
-
-plot(roc_obj, col = "blue", lwd = 2, main = "ROC Curve")
-
-# Add diagonal line representing random classifier
-lines(x = c(0, 1), y = c(0, 1), col = "red", lty = 2)
-
-# Calculate AUC
-auc <- roc_obj$auc
-
-# Print AUC value
-text(0.7, 0.2, paste("AUC = ", round(auc, 2)), col = "blue", cex = 1.2)
 
 
-
-##:::KRYSTAL TESTING :::##
-
-### if we use dec2023 and jan2024 to train, feb2024 to test
-training_set <- rbind(pv_train_202312, pv_train_202401)
-training_set$YEAR_MONTH <- as.Date(paste0(training_set$YEAR_MONTH, "-01"))
-training_set_encoded <- encode_categorical(training_set, "DAY_TYPE")
-training_set_encoded <- encode_categorical(training_set_encoded, "DESTINATION_PT_CODE")
-training_set_encoded <- encode_categorical(training_set_encoded, "ORIGIN_PT_CODE")
-
-training_set_encoded <- training_set_encoded %>%
-  select(-DESTINATION_PT_CODE, -ORIGIN_PT_CODE, -PT_TYPE, -DAY_TYPE, -YEAR_MONTH)
-
-X <- as.matrix(training_set_encoded[, setdiff(names(training_set_encoded), "TOTAL_TRIPS")])
-y <- training_set_encoded[["TOTAL_TRIPS"]]
-
-X_array <- array(X, dim = c(nrow(X), 1, ncol(X)))
-
-test_set <- test_202402
-test_set$YEAR_MONTH <- as.Date(paste0(test_set$YEAR_MONTH, "-01"))
-test_set_encoded <- encode_categorical(test_set, "DAY_TYPE")
-test_set_encoded <- encode_categorical(test_set_encoded, "DESTINATION_PT_CODE")
-test_set_encoded <- encode_categorical(test_set_encoded, "ORIGIN_PT_CODE")
-
-test_set_encoded <- test_set_encoded %>%
-  select(-DESTINATION_PT_CODE, -ORIGIN_PT_CODE, -PT_TYPE, -DAY_TYPE, -YEAR_MONTH)
-
-X_test <- as.matrix(test_set_encoded[, setdiff(names(test_set_encoded), "TOTAL_TRIPS")])
-y_test <- test_set_encoded[["TOTAL_TRIPS"]]
-
-X_test_array <- array(X_test, dim = c(nrow(X_test), 1, ncol(X_test)))
-
-model <- keras_model_sequential() %>%
-  layer_lstm(units = 50, input_shape = c(1, ncol(X))) %>%
-  layer_dense(units = 1)
-
-# Compile the model
-model %>% compile(
-  optimizer = 'adam',
-  loss = 'mean_squared_error',
-  metrics = c('mean_absolute_error')
-)
-
-# Train the model
-history <- model %>% fit(
-  X_array, y,
-  epochs = 20, 
-  batch_size = 128,
-  validation_split = 0.2,
-  verbose = 1
-)
-
-
-model %>% evaluate(X_test_array, 
-                   y_test, 
-                   verbose = 1)
-
-
-y_pred <- model %>% predict(X_test_array)   
-RMSE <- sqrt(mean((y_test - y_pred)^2)) 
-cat("Root Mean Squared Error (RMSE):", round(RMSE, 2), "\n")
-# Root Mean Squared Error (RMSE): 416.61 << worse, overfitting
-
-##:::KRYSTAL TESTING :::## 
 
 
 
